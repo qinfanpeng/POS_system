9131cad80867d8730bfa93fe387240a32eba9516
Yan Qian <qianyan@thoughtworks.com>
2014-11-23 09:51:36 +0800

    [yan qian] refactor discount.

--- src/main/java/com/tw/pos/Discount.java
+++ src/main/java/com/tw/pos/Discount.java
@@ -1,20 +1,18 @@
 package com.tw.pos;
 
-/**
- * Created by fpqin on 14/11/20.
- */
 public class Discount implements Promotion{
     private double rate;
 
     public Discount(double rate) {
-        if (rate < 0.0 || rate > 10.0 || moreThanTwoDigit(rate)) {
+        if (rate < 0.0 || rate > 10.0 || moreThanOneDecimal(rate)) {
             throw new IllegalArgumentException("Discount rate should be between 0.0 to 10.0, like 5 or 7.5!");
         }
         this.rate = rate;
     }
 
-    private boolean moreThanTwoDigit(double rate) {
-        return Double.toString(rate).length() > 3;
+    private boolean moreThanOneDecimal(double rate) {
+        String number = Double.toString(rate);
+        return number.substring(number.indexOf('.')+1).length() > 1;
     }
 
     @Override
--- src/test/java/com/tw/pos/DiscountTest.java
+++ src/test/java/com/tw/pos/DiscountTest.java
@@ -12,13 +12,19 @@ public class DiscountTest {
     public ExpectedException expectedException = ExpectedException.none();
 
     @Test
-    public void should_be_ensure_in_between_0_and_10() throws Exception {
+    public void should_be_ensure_less_than_10() throws Exception {
         expectedException.expectMessage("Discount rate should be between 0.0 to 10.0, like 5 or 7.5!");
         new Discount(11.1);
     }
 
     @Test
-    public void should_be_between_0_to_10() throws Exception {
+    public void should_be_ensure_greater_than_0() throws Exception {
+        expectedException.expectMessage("Discount rate should be between 0.0 to 10.0, like 5 or 7.5!");
+        new Discount(-0.1);
+    }
+
+    @Test
+    public void should_not_only_have_one_decimal() throws Exception {
         expectedException.expectMessage("Discount rate should be between 0.0 to 10.0, like 5 or 7.5!");
         new Discount(5.55);
     }
